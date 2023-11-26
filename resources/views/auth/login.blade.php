9efa42950f8874365dcc33cfce4d155b1aee5b28
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-11-26 21:14:12 +0700

    57. 2_Frontend - Login Page Mastering (Part - 2)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,16 +1,17 @@
 @extends('frontend.layouts.master')
+
 @section('content')
     <!--=============================
             BREADCRUMB START
         ==============================-->
-    <section class="fp__breadcrumb" style="background: url(images/counter_bg.jpg);">
+    <section class="fp__breadcrumb" style="background: url({{ config('settings.breadcrumb') }});">
         <div class="fp__breadcrumb_overlay">
             <div class="container">
                 <div class="fp__breadcrumb_text">
                     <h1>sign in</h1>
                     <ul>
-                        <li><a href="index.html">home</a></li>
-                        <li><a href="#">sign in</a></li>
+                        <li><a href="{{ url('/') }}">home</a></li>
+                        <li><a href="javascript:;">sign in</a></li>
                     </ul>
                 </div>
             </div>
@@ -32,32 +33,37 @@
                         <div class="fp__login_area">
                             <h2>Welcome back!</h2>
                             <p>sign in to continue</p>
-                            <form>
+                            <form action="{{ route('login') }}" method="POST">
+                                @csrf
                                 <div class="row">
                                     <div class="col-xl-12">
                                         <div class="fp__login_imput">
                                             <label>email</label>
-                                            <input type="email" placeholder="Email">
+                                            <input type="email" name="email" placeholder="Email" required
+                                                value="{{ old('email') }}">
                                         </div>
                                     </div>
+
                                     <div class="col-xl-12">
                                         <div class="fp__login_imput">
                                             <label>password</label>
-                                            <input type="password" placeholder="Password">
+                                            <input type="password" placeholder="Password" name="password">
                                         </div>
                                     </div>
+
                                     <div class="col-xl-12">
                                         <div class="fp__login_imput fp__login_check_area">
                                             <div class="form-check">
                                                 <input class="form-check-input" type="checkbox" value=""
-                                                    id="flexCheckDefault">
+                                                    id="flexCheckDefault" name="remember">
                                                 <label class="form-check-label" for="flexCheckDefault">
                                                     Remeber Me
                                                 </label>
                                             </div>
-                                            <a href="forgot_password.html">Forgot Password ?</a>
+                                            <a href="{{ route('password.request') }}">Forgot Password ?</a>
                                         </div>
                                     </div>
+
                                     <div class="col-xl-12">
                                         <div class="fp__login_imput">
                                             <button type="submit" class="common_btn">login</button>
@@ -66,13 +72,9 @@
                                 </div>
                             </form>
                             <p class="or"><span>or</span></p>
-                            <ul class="d-flex">
-                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
-                            </ul>
-                            <p class="create_account">Dont’t have an aceount ? <a href="sign_up.html">Create Account</a>
+
+                            <p class="create_account">Dont’t have an aceount ? <a href="{{ route('register') }}">Create
+                                    Account</a>
                             </p>
                         </div>
                     </div>
