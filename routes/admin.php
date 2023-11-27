40f82dc66a843e051336a18716088da6bfaced0f
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-11-27 15:42:40 +0700

    80. 1_Home Slider- Creating Necessary files

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\AdminDashboardController;
 use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Admin\SliderController;
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function() {
 
@@ -11,5 +12,10 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function() {
     Route::get('profile', [ProfileController::class, 'index'])->name('profile');
     Route::put('profile', [ProfileController::class, 'updateProfile'])->name('profile.update');
     Route::put('profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password.update');
+
+    /** Slider Routes */
+    Route::resource('slider', SliderController::class);
+
+
 
 });
