ca7dfb0414b9c46e2739c8789c83677725522458
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-11-27 23:55:49 +0700

    109. 12_Why Choose Us - Showing Dynamic Contents at Frontend

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -3,14 +3,40 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\SectionTitle;
 use App\Models\Slider;
+use App\Models\WhyChooseUs;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 class FrontendController extends Controller
 {
     function index() : View {
+        $sectionTitles = $this->getSectionTitles();
+
         $sliders = Slider::where('status', 1)->get();
-        return view('frontend.home.index', compact('sliders'));
+        $whyChooseUs = WhyChooseUs::where('status', 1)->get();
+        return view('frontend.home.index', compact('sliders', 'sectionTitles', 'whyChooseUs'));
+    }
+
+    function getSectionTitles(): Collection
+    {
+        $keys = [
+            'why_choose_top_title',
+            'why_choose_main_title',
+            'why_choose_sub_title',
+            'daily_offer_top_title',
+            'daily_offer_main_title',
+            'daily_offer_sub_title',
+            'chef_top_title',
+            'chef_main_title',
+            'chef_sub_title',
+            'testimonial_top_title',
+            'testimonial_main_title',
+            'testimonial_sub_title'
+        ];
+
+        return SectionTitle::whereIn('key', $keys)->pluck('value', 'key');
     }
 }
